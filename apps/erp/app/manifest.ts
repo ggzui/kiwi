8cb2a851abb60e3d164bc420c33ac1bd7532cf18
hzp <zhoupingh@outlook.com>
2024-06-07 16:37:00 +0800

    refactor(ui): custom app metadata

--- apps/erp/app/manifest.ts
+++
@@ -1,9 +0,0 @@
-import type { MetadataRoute } from 'next'
-
-export default function manifest(): MetadataRoute.Manifest {
-  return {
-    name: 'Next.js App',
-    short_name: 'Next.js App',
-    description: 'Next.js App',
-  }
-}
--- apps/erp/app/page.tsx
+++ apps/erp/app/page.tsx
@@ -1,4 +1,10 @@
 import { Button } from '@kiwi/ui/button'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Next.js App',
+  description: 'A Next.js app with a custom domain',
+}
 
 export default function Home() {
   return (
